62235b58d478a6cdf30bee25343b7caca68765a6
John <playingallday383@gmail.com>
2025-09-10 20:09:32 -0400

    Shell progress

--- src/shell/index.ts
+++ src/shell/index.ts
@@ -1,4 +1,4 @@
-import { createFSError } from "../fs/errors";
+import { createFSError, genError } from "../fs/errors";
 import { Path } from "../path/index";
 import { FS } from "../fs/index";
 
@@ -26,11 +26,72 @@ export class Shell {
 		});
 	}
 
+	pwd() {
+		return this.cwd;
+	}
+
+	cat(path: string | string[], callback: (error: Error | null, data: string | null) => void) {
+		const paths = Array.isArray(path) ? path : [path];
+		const fp = paths.map(p => this.path.join(this.cwd, p));
+		let results: string[] = [];
+		let completed = 0;
+		if (fp.length === 0) {
+			callback(genError("NotFoundError"), null);
+			return;
+		}
+		fp.forEach((p, idx) => {
+			this.fs.readFile(p, "utf8", (err: Error | null, data: string | null) => {
+				if (err) {
+					callback(genError(err), null);
+				} else {
+					results[idx] = data as string;
+				}
+				completed++;
+				if (completed === fp.length) {
+					callback(null, results.join(""));
+				}
+			});
+		});
+	}
+
+	ls(path: string, callback: (error: Error | null, entries: string[] | null) => void) {
+		const newPath = this.path.join(this.cwd, path);
+		this.fs.readdir(newPath, (err, entries) => {
+			if (err) {
+				callback(genError(err), null);
+			} else {
+				callback(null, entries as string[]);
+			}
+		});
+	}
+
 	promises = {
 		cd: (path: string): Promise<void> => {
 			return new Promise(resolve => {
 				this.cd(path);
 				resolve();
+			});
+		},
+		cat: (path: string): Promise<string> => {
+			return new Promise((resolve, reject) => {
+				this.cat(path, (err, data) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(data as string);
+					}
+				});
+			});
+		},
+		ls: (path: string): Promise<string[]> => {
+			return new Promise((resolve, reject) => {
+				this.ls(path, (err, entries) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(entries as string[]);
+					}
+				});
 			});
 		},
 	};
